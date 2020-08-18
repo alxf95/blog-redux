c86d158961362aee431406ee477dd6ea32fe2e7a
Alex Fiennes <alexfiennes246@gmail.com>
2020-08-18 13:35:22 +0100

    initial commit for redux blog

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import postsReducer from './reducers/postsReducer';
+import PostsIndex from './containers/PostsIndex';
+import './application.css';
+
+const reducers = combineReducers({
+  posts: postsReducer,
+});
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+// render an instance of the component in the DOM
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStore(reducers, {}, middlewares)}>
+    <BrowserRouter>
+      <Switch>
+        <div className="ui container">
+          <Switch>
+            <Route path="/" exact component={PostsIndex} />
+          </Switch>
+        </div>
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
